99ed963f09c56da7efd68f1442ce7c2dba1858ac
Gilles Vink <gilles@meneervink.nl>
2022-08-25 15:56:59 +0200

    Optimized submission

--- python/tk_multi_deadlinereviewsubmission/handler.py
+++ python/tk_multi_deadlinereviewsubmission/handler.py
@@ -23,6 +23,7 @@
 import sgtk
 import os
 import tempfile
+from shutil import rmtree
 from subprocess import check_output
 
 # standard toolkit logger
@@ -32,24 +33,39 @@ logger = sgtk.platform.get_logger(__name__)
 class DeadlineReviewSubmissionHandler:
     def __init__(self):
         self.__app = sgtk.platform.current_bundle()
+        self.deadline_command = os.getenv("DEADLINE_PATH")
 
     def submit_to_deadline(
         self,
-        template=None,
-        fields=None,
-        sg_publishes=None,
-        start_frame=None,
-        end_frame=None,
-        fps=None,
-        filename=None,
+        template,
+        fields,
+        sg_publishes,
+        start_frame,
+        end_frame,
+        fps,
+        filename,
         comment=None,
     ):
+        """Get parameters from publisher, translate them to text and
+        submit using deadline command
 
-        ### Getting attached data from the publish plugin
+        Args:
+            template (attribute): publisher template
+            fields (attribute): fields from render path
+            sg_publishes (list): data created by publisher
+            start_frame (int): first frame for sequence
+            end_frame (_type_): last frame for sequence
+            fps (int): frames per second used in project
+            filename (str): script name current file
+            comment (str, optional): any comment made by artist.
+            Defaults to None.
+        """
         # Building template to get sequence path
         fields["SEQ"] = "FORMAT: %d"
         sequence_path = template.apply_fields(fields)
+
         for publish in sg_publishes:
+
             # Getting current project id and project name
             project_id = publish.get("project").get("id")
             project_name = publish.get("project").get("name")
@@ -69,7 +85,7 @@ class DeadlineReviewSubmissionHandler:
             # Getting publish id
             publish_id = publish.get("id")
 
-            ### Getting settings from the app configuration
+            # Getting settings from the app configuration
             priority = self.__app.get_setting("default_priority")
             company_name = self.__app.get_setting("company_name")
 
@@ -79,68 +95,205 @@ class DeadlineReviewSubmissionHandler:
             if comment is None:
                 comment = " "
 
-            ### Getting system data
-            # Location of deadline installation
-            deadline_path = os.getenv("DEADLINE_PATH")
+            department = "ShotGrid"
+            plugin = "ShotGridReview"
 
-            ### Building all files
-            # Building job info properties
-            job_info = [
-                "Name=" + filename,
-                "Plugin=ShotGridReview",
-                "Department=ShotGrid",
-                "Priority=" + str(priority),
-            ]
+            # Every argument has to be split because we are
+            # sending it via deadlinecommand
+            submission_parameters = self.__get_submission_parameters(
+                plugin=plugin,
+                priority=priority,
+                department=department,
+                filename=filename,
+                start_frame=start_frame,
+                end_frame=end_frame,
+                fps=fps,
+                project_id=project_id,
+                user_id=user_id,
+                entity_type=entity_type,
+                publish_id=publish_id,
+                task_id=task_id,
+                sequence_path=sequence_path,
+                render_path=render_path,
+                company_name=company_name,
+                project_name=project_name,
+                user_name=user_name,
+                comment=comment,
+                entity_version=entity_version,
+            )
 
-            # Building plugin info properties
-            plugin_info = [
-                "Version=1",
-                "StartFrame=" + str(start_frame),
-                "EndFrame=" + str(end_frame),
-                "FPS=" + str(fps),
-                "ProjectID=" + str(project_id),
-                "UserID=" + str(user_id),
-                "EntityType=" + entity_type,
-                "EntityID=" + str(entity_id),
-                "PublishID=" + str(publish_id),
-                "TaskID=" + str(task_id),
-                "SequenceFile=" + sequence_path,
-                "OutputFile=" + render_path,
-                "CompanyName=" + company_name,
-                "ProjectName=" + project_name,
-                "Artist=" + user_name,
-                "Description=" + comment,
-                "ShotGridVersion=" + str(entity_version),
-                "FileName=" + filename,
-            ]
+            # Submit to Deadline
+            self.__submit_to_deadline(submission_parameters)
 
-            # Create temporary directory to create job info and plugin files for submission
-            temporary_directory = tempfile.mkdtemp()
+    def __get_submission_parameters(
+        self,
+        plugin,
+        priority,
+        department,
+        filename,
+        start_frame,
+        end_frame,
+        fps,
+        project_id,
+        user_id,
+        entity_type,
+        publish_id,
+        task_id,
+        sequence_path,
+        render_path,
+        company_name,
+        project_name,
+        user_name,
+        comment,
+        entity_version,
+    ):
+        """
+        Create dictionaries containing all submission parameters
+        the panel gives.
+        Will return a dictionary:
+            {
+            "job_info": {
+                "Plugin": "Nuke",
+                "Frames": "1-100",
+                "Priority": 70,
+                "Name": "ExampleScript",
+                "Department": "Compositing",
+                "ConcurrentTasks": 10,
+                "ChunkSize": 3,
+                "OutputDirectory0": "/example/path",
+                "OutputFilename0": "file.exr",
+            },
+            "plugin_info": {
+                "Version": "13.2",
+                "WriteNode": "Write1",
+                "SceneFile": "/example/path/ExampleScript.nk",
+            },
+        }
+        """
 
-            # Writing job_info.txt
-            job_info_filepath = os.path.join(
-                temporary_directory, "job_info.txt"
-            ).replace(os.sep, "/")
-            job_info_textfile = open(job_info_filepath, "w")
-            for item in job_info:
-                job_info_textfile.write(item + "\n")
-            job_info_textfile.close()
+        # Getting job submission parameters
+        job_info = {}
+        job_info["Plugin"] = plugin
+        job_info["Priority"] = priority
+        job_info["Name"] = filename
+        job_info["Department"] = department
+        job_info["OutputDirectory0"] = os.path.dirname(render_path)
+        job_info["OutputFilename0"] = os.path.basename(render_path)
 
-            # Writing plugin_info.txt
-            plugin_info_filepath = os.path.join(
-                temporary_directory, "plugin_info.txt"
-            ).replace(os.sep, "/")
-            plugin_info_textfile = open(plugin_info_filepath, "w")
-            for item in plugin_info:
-                plugin_info_textfile.write(item + "\n")
-            plugin_info_textfile.close()
+        # Getting plugin submission parameters
+        plugin_info = {}
+        plugin_info["Version"] = 1
+        plugin_info["StartFrame"] = start_frame
+        plugin_info["EndFrame"] = end_frame
+        plugin_info["FPS"] = fps
+        plugin_info["ProjectID"] = project_id
+        plugin_info["UserID"] = user_id
+        plugin_info["EntityType"] = entity_type
+        plugin_info["PublishID"] = publish_id
+        plugin_info["TaskID"] = task_id
+        plugin_info["SequenceFile"] = sequence_path
+        plugin_info["OutputFile"] = render_path
+        plugin_info["CompanyName"] = company_name
+        plugin_info["ProjectName"] = project_name
+        plugin_info["Artist"] = user_name
+        plugin_info["Description"] = comment
+        plugin_info["ShotGridVersion"] = entity_version
+        plugin_info["FileName"] = filename
 
+        # Create dictionary containing both dictionaries
+        submission_files = {
+            "job_info": job_info,
+            "plugin_info": plugin_info,
+        }
+
+        return submission_files
+
+    def __submit_to_deadline(self, submission_parameters):
+        """
+        This function will create using the provided submission
+        dictionary both the job_info.txt file and plugin_info.txt.
+        This will be done in a temporary directory.
+        After the creation of these files, we call the deadlinecommand
+        and submit the job to Deadline.
+        """
+
+        # First we will create the necessary files to submit
+        # These are the job_info.txt and plugin_info.txt
+        # After submitting these files Deadline will understand the submission
+
+        submission_files = []
+
+        # Setting initial message in case something went wrong
+        # We will change this variable if submission succeeded
+        result = "Something went wrong"
+
+        # Create temporary directory for submission text files
+        temporary_directory = tempfile.mkdtemp()
+
+        # We run the code within a try and except to
+        # catch any exceptions and give them to the user
+        try:
+
+            # Iterating through the provided dictionary, and get
+            # the created dictionaries inside.
+            for submission_info in submission_parameters.keys():
+
+                # Get the parameters for the specified dictionary
+                # (for example job_info)
+                info_parameters = submission_parameters.get(submission_info)
+
+                # Create the path for the info_file.txt, for example
+                # path/to/temporarydirectory/job_info.txt
+                info_file = os.path.join(
+                    temporary_directory,
+                    submission_info + ".txt",
+                )
+
+                # Fix for Windows backward slashes systems
+                info_file = info_file.replace(os.sep, "/")
+
+                # Create a text file
+                info_file_txt = open(info_file, "w", encoding="utf-8")
+
+                # Iterate through the dictionary for every key
+                for parameter in info_parameters.keys():
+
+                    # Get the value per parameter
+                    value = info_parameters.get(parameter)
+
+                    # Write the key and value and create a new line
+                    # For example:
+                    # Plugin=Nuke (key=value)
+                    info_file_txt.write(parameter + "=" + str(value) + "\n")
+
+                # Close the file to write parameters
+                info_file_txt.close()
+
+                # Add the created text file to the list for submission
+                submission_files.append(info_file)
+
+            # Create the command for calling deadline
             deadline_command = [
-                os.path.join(deadline_path, "deadlinecommand"),
-                job_info_filepath,
-                plugin_info_filepath,
+                os.path.join(self.deadline_command, "deadlinecommand")
             ]
 
-            execute_submission = check_output(deadline_command)
+            # Append the text files for the submission parameters
+            deadline_command = deadline_command + submission_files
 
-            return render_path
+            # Create a subprocess using deadlinecommand and run the submission
+            submission = check_output(deadline_command)
+
+            # Return the command output so the user
+            # will get the submission information
+            result = str(submission)
+
+        # If there is an error, return the error
+        # so the user knows
+        except Exception as error:
+            result = str(error)
+
+        # Always remove the created temporary directory
+        finally:
+            rmtree(temporary_directory)
+
+        return result
